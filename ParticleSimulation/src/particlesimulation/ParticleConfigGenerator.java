9506a159754375a2f9c0e3bb16605b07384c1bdb
akerue <hsmgn8118akerue@gmail.com>
2014-05-24 15:06:39 +0900

    Attach initial velocity

---
+++ ParticleSimulation/src/particlesimulation/ParticleConfigGenerator.java
@@ -0,0 +1,50 @@
+package particlesimulation;
+import java.util.Random;
+import java.io.*;
+import ParticleSimulation.*;
+
+public class ParticleConfigGenerator {
+	static Random random = new Random();
+	public static final int NUM = 100;
+	public static final int WIDTH = (int) ParticleSimulation.WIDTH;
+	public static final int MAX_VELOCITY = 50;
+
+	public static void main(String[] args) throws IOException {
+		double x, y, z;
+		double v_x, v_y, v_z;
+		String[] pos;
+		String line;
+		
+		String filepath = new String("/Users/Robbykunsan/Workspace/mlab/training/config/conf.txt");
+		File conf_file = new File(filepath);
+		BufferedWriter bw = new BufferedWriter(new FileWriter(conf_file));
+
+		try {
+			for (int i = 0; i<NUM ; i++) {
+				x = random.nextInt(WIDTH) + random.nextDouble();
+				y = random.nextInt(WIDTH) + random.nextDouble();
+				z = random.nextInt(WIDTH) + random.nextDouble();
+				v_x = random.nextInt(MAX_VELOCITY * 2) + random.nextDouble() - MAX_VELOCITY;
+				v_y = random.nextInt(MAX_VELOCITY * 2) + random.nextDouble() - MAX_VELOCITY;
+				v_z = random.nextInt(MAX_VELOCITY * 2) + random.nextDouble() - MAX_VELOCITY;
+				
+				pos = new String[]{Double.toString(x), 
+						   Double.toString(y), 
+						   Double.toString(z),
+						   Double.toString(v_x), 
+						   Double.toString(v_y), 
+						   Double.toString(v_z)};
+				StringBuilder builder = new StringBuilder();
+				for (int j = 0; j < pos.length - 1; j++){
+					builder.append(pos[j]).append(" ");
+				}
+				line = builder.append(pos[pos.length-1]).toString();
+				bw.write(line);
+				bw.newLine();
+			}
+		}finally{
+			bw.close();
+		}
+	}
+	
+}
